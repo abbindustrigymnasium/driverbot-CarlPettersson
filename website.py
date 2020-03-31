6b89cdbf9ad66f9e1495c95d2cef49653ec87ed2
CarlPettersson <54657589+CarlPettersson@users.noreply.github.com>
2020-03-31 10:53:26 +0200

    Update website.py

--- website.py
+++ website.py
@@ -1,9 +1,30 @@
 import pygame
 pygame.init()
 
+import paho.mqtt.client as mqtt
+import time
+
 display_width = 1100
 display_height = 600
 display = pygame.display.set_mode((display_width, display_height))
+
+def on_log(client, userdata, level, buf):
+    print("log: " + buf)
+
+def on_connect (client, userdata, flags, rc):
+    if rc == 0:
+        print("connected OK")
+    else:
+        print("bad connection returned code=", rc)
+
+def on_disconnect(client, userdata, flags, rc=0):
+    print("DisConnected result code " + str(rc))
+
+port = 8883
+broker = "maqiatto.com"
+client =mqtt.Client("py1")
+MQTT_USERNAME  = "carl.pettersson@abbindustrigymnasium.se"
+MQTT_PASSWORD  = "Snuten12"
 
 run = True
 
@@ -11,7 +32,6 @@ while run:
     for event in pygame.event.get():  
         if event.type == pygame.QUIT:
             run = False
-
 
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
@@ -21,10 +41,42 @@ while run:
             if click[0] == True:
                 print("forward = True")
 
+                client.on_connect=on_connect
+                client.on_disconnect=on_disconnect
+                client.on_log=on_log
+                client.username_pw_set(username=MQTT_USERNAME, password=MQTT_PASSWORD)
+
+                print("connecting to broker ", broker, )
+
+                client.connect(broker, port=1883)
+                client.loop_start()
+                client.subscribe("carl.pettersson@abbindustrigymnasium.se/drive", 1)
+                client.publish("carl.pettersson@abbindustrigymnasium.se/drive", "True")
+
+                # time.sleep(4)
+                client.loop_stop()
+                client.disconnect()
+
     if mouse[0] > display_width/2 + 100 and mouse[0] < display_width/2 + 300:
         if mouse[1] > display_height/2 - 50 and mouse[1] < display_height/2 + 50:
             if click[0] == True:
                 print("forward = False")
+
+                client.on_connect=on_connect
+                client.on_disconnect=on_disconnect
+                client.on_log=on_log
+                client.username_pw_set(username=MQTT_USERNAME, password=MQTT_PASSWORD)
+
+                print("connecting to broker ", broker, )
+
+                client.connect(broker, port=1883)
+                client.loop_start()
+                client.subscribe("carl.pettersson@abbindustrigymnasium.se/drive", 1)
+                client.publish("carl.pettersson@abbindustrigymnasium.se/drive", "False")
+
+                # time.sleep(4)
+                client.loop_stop()
+                client.disconnect()
 
     display.fill((255, 255, 255))
 
