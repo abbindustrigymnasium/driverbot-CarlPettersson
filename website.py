942b01420b7e02f7b477f6c17e9b80e250a8038c
CarlPettersson <54657589+CarlPettersson@users.noreply.github.com>
2020-05-11 12:51:04 +0200

    Servo control

--- website.py
+++ website.py
@@ -23,29 +23,57 @@ def on_disconnect(client, userdata, flags, rc=0):
 def draw(Direction):
     display.fill((255, 255, 255))
 
-    pygame.draw.rect(display, (0, 150, 0), (display_width/2 - 296, display_height/2 - 54, 200, 100))  
-    pygame.draw.rect(display, (0, 150, 0), (display_width/2 - 297, display_height/2 - 53, 200, 100)) # ritar buttons och gör bakgrunden vit
-    pygame.draw.rect(display, (0, 150, 0), (display_width/2 - 298, display_height/2 - 52, 200, 100)) # ritar också texten
-    pygame.draw.rect(display, (0, 150, 0), (display_width/2 - 299, display_height/2 - 51, 200, 100))
-    pygame.draw.rect(display, (0, 255, 0), (display_width/2 - 300, display_height/2 - 50, 200, 100))
+    pygame.draw.rect(display, (0, 150, 0), (display_width/2 - 346, display_height/2 + 54, 200, 100))  
+    pygame.draw.rect(display, (0, 150, 0), (display_width/2 - 347, display_height/2 + 53, 200, 100)) # ritar buttons och gör bakgrunden vit
+    pygame.draw.rect(display, (0, 150, 0), (display_width/2 - 348, display_height/2 + 52, 200, 100)) # ritar också texten
+    pygame.draw.rect(display, (0, 150, 0), (display_width/2 - 349, display_height/2 + 51, 200, 100))
+    pygame.draw.rect(display, (0, 255, 0), (display_width/2 - 350, display_height/2 + 50, 200, 100))
 
     green_button_text = pygame.font.SysFont('arial', 48)
     text = green_button_text.render('Forward', 1, (0,0,0))
-    display.blit(text, (display_width/2 - 275, display_height/2 + 70))
+    display.blit(text, (display_width/2 - 325, display_height/2 + 75))
 
-    pygame.draw.rect(display, (150, 0, 0), (display_width/2 + 96, display_height/2 - 54, 200, 100))  
-    pygame.draw.rect(display, (150, 0, 0), (display_width/2 + 97, display_height/2 - 53, 200, 100))
-    pygame.draw.rect(display, (150, 0, 0), (display_width/2 + 98, display_height/2 - 52, 200, 100))
-    pygame.draw.rect(display, (150, 0, 0), (display_width/2 + 99, display_height/2 - 51, 200, 100))
-    pygame.draw.rect(display, (255, 0, 0), (display_width/2 + 100, display_height/2 - 50, 200, 100))
+    pygame.draw.rect(display, (150, 0, 0), (display_width/2 + 146, display_height/2 + 54, 200, 100))  
+    pygame.draw.rect(display, (150, 0, 0), (display_width/2 + 147, display_height/2 + 53, 200, 100))
+    pygame.draw.rect(display, (150, 0, 0), (display_width/2 + 148, display_height/2 + 52, 200, 100))
+    pygame.draw.rect(display, (150, 0, 0), (display_width/2 + 149, display_height/2 + 51, 200, 100))
+    pygame.draw.rect(display, (255, 0, 0), (display_width/2 + 150, display_height/2 + 50, 200, 100))
 
     green_button_text = pygame.font.SysFont('arial', 48)
     text = green_button_text.render('Backward', 1, (0,0,0))
-    display.blit(text, (display_width/2 + 115, display_height/2 + 70))
+    display.blit(text, (display_width/2 + 165, display_height/2 + 75))
+
+    pygame.draw.rect(display, (150, 150, 0), (display_width/2 - 346, 100, 200, 100))  
+    pygame.draw.rect(display, (150, 150, 0), (display_width/2 - 347, 101, 200, 100)) # ritar buttons och gör bakgrunden vit
+    pygame.draw.rect(display, (150, 150, 0), (display_width/2 - 348, 102, 200, 100)) # ritar också texten
+    pygame.draw.rect(display, (150, 150, 0), (display_width/2 - 349, 103, 200, 100))
+    pygame.draw.rect(display, (255, 255, 0), (display_width/2 - 350, 104, 200, 100))
 
     green_button_text = pygame.font.SysFont('arial', 48)
+    text = green_button_text.render('Left', 1, (0,0,0))
+    display.blit(text, (display_width/2 - 300, 130))
+
+    pygame.draw.rect(display, (150, 150, 0), (display_width/2 + 146, 100, 200, 100))  
+    pygame.draw.rect(display, (150, 150, 0), (display_width/2 + 147, 101, 200, 100))
+    pygame.draw.rect(display, (150, 150, 0), (display_width/2 + 148, 102, 200, 100))
+    pygame.draw.rect(display, (150, 150, 0), (display_width/2 + 149, 103, 200, 100))
+    pygame.draw.rect(display, (255, 255, 0), (display_width/2 + 150, 104, 200, 100))
+
+    green_button_text = pygame.font.SysFont('arial', 48)
+    text = green_button_text.render('Right', 1, (0,0,0))
+    display.blit(text, (display_width/2 + 190, 130))
+
+    pygame.draw.rect(display, (150, 150, 0), (display_width/2 - 103, display_height/2 - 73, 206, 106))
+    pygame.draw.rect(display, (255, 255, 0), (display_width/2 - 100, display_height/2 - 70, 200, 100))
+
+    green_button_text = pygame.font.SysFont('arial', 48)
+    text = green_button_text.render('Straight', 1, (0,0,0))
+    display.blit(text, (display_width/2 - 75, display_height/2 - 50))
+
+    green_button_text = pygame.font.SysFont('arial', 20)
     text = green_button_text.render('Direction: ' + Direction, 1, (0,0,0))
     display.blit(text, (20, 20))
+
     pygame.display.update()
 
 port = 8883
@@ -69,9 +97,12 @@ while run:
     mouse = pygame.mouse.get_pos()   #får mus positionen
     click = pygame.mouse.get_pressed() #gör så att man vet om musen är tryckt och vilken knapp man tryckte
 
-    if mouse[0] > display_width/2 - 300 and mouse[0] < display_width/2 - 100:
-        if mouse[1] > display_height/2 - 50 and mouse[1] < display_height/2 + 50:
-            if click[0] == True:
+    keys = pygame.key.get_pressed()
+
+    if mouse[0] > display_width/2 - 350 and mouse[0] < display_width/2 - 150 or keys[pygame.K_w]:
+        if mouse[1] > display_height/2 + 50 and mouse[1] < display_height/2 + 150 or keys[pygame.K_w]:
+            print("forward")
+            if click[0] == True or keys[pygame.K_w]:
                 print("forward = True")   #gör så att om musen trycker på en "knapp" så kommer Direction säga forward
                 Direction = "forward"  #och hjulen på bilen kommer börja snurra framåt. 
                 draw(Direction)    
@@ -91,9 +122,10 @@ while run:
                 client.loop_stop()
                 client.disconnect()    #disconnectar till broker
 
-    if mouse[0] > display_width/2 + 100 and mouse[0] < display_width/2 + 300:
-        if mouse[1] > display_height/2 - 50 and mouse[1] < display_height/2 + 50:   #"knappen" area
-            if click[0] == True:
+    if mouse[0] > display_width/2 + 150 and mouse[0] < display_width/2 + 350 or keys[pygame.K_s]:
+        if mouse[1] > display_height/2 + 50 and mouse[1] < display_height/2 + 150 or keys[pygame.K_s]:   #"knappen" area
+            print("backward")
+            if click[0] == True or keys[pygame.K_s]:
                 print("forward = False")
                 Direction = "backward"          #samma som över men backwards
                 draw(Direction)
@@ -109,6 +141,75 @@ while run:
                 client.loop_start()
                 client.subscribe("carl.pettersson@abbindustrigymnasium.se/drive", 1)
                 client.publish("carl.pettersson@abbindustrigymnasium.se/drive", "F", 0)
+
+                client.loop_stop()
+                client.disconnect()
+
+    if mouse[0] > display_width/2 + 150 and mouse[0] < display_width/2 + 350 or keys[pygame.K_d]:
+        if mouse[1] > 104 and mouse[1] < 204 or keys[pygame.K_d]:   #"knappen" area
+            print("Right")
+            if click[0] == True or keys[pygame.K_d]:
+                print("forward = False")
+                Direction = Direction          #samma som över men backwards
+                draw(Direction)
+
+                client.on_connect=on_connect
+                client.on_disconnect=on_disconnect
+                client.on_log=on_log
+                client.username_pw_set(username=MQTT_USERNAME, password=MQTT_PASSWORD)
+
+                print("connecting to broker ", broker, )
+
+                client.connect(broker, port=1883)
+                client.loop_start()
+                client.subscribe("carl.pettersson@abbindustrigymnasium.se/drive", 1)
+                client.publish("carl.pettersson@abbindustrigymnasium.se/drive", "R", 0)
+
+                client.loop_stop()
+                client.disconnect()
+
+    if mouse[0] > display_width/2 - 350 and mouse[0] < display_width/2 - 150 or keys[pygame.K_a]:
+        if mouse[1] > 104 and mouse[1] < 204 or keys[pygame.K_a]:   #"knappen" area
+            print("Left")
+            if click[0] == True or keys[pygame.K_a]:
+                print("forward = False")
+                Direction = Direction          #samma som över men backwards
+                draw(Direction)
+
+                client.on_connect=on_connect
+                client.on_disconnect=on_disconnect
+                client.on_log=on_log
+                client.username_pw_set(username=MQTT_USERNAME, password=MQTT_PASSWORD)
+
+                print("connecting to broker ", broker, )
+
+                client.connect(broker, port=1883)
+                client.loop_start()
+                client.subscribe("carl.pettersson@abbindustrigymnasium.se/drive", 1)
+                client.publish("carl.pettersson@abbindustrigymnasium.se/drive", "L", 0)
+
+                client.loop_stop()
+                client.disconnect()
+
+    if mouse[0] > display_width/2 - 100 and mouse[0] < display_width/2 + 100 or keys[pygame.K_SPACE]:
+        if mouse[1] > display_height/2 - 70 and mouse[1] < display_height/2 + 30 or keys[pygame.K_SPACE]:   #"knappen" area
+            print("Straight")
+            if click[0] == True or keys[pygame.K_SPACE]:
+                print("forward = False")
+                Direction = Direction          #samma som över men backwards
+                draw(Direction)
+
+                client.on_connect=on_connect
+                client.on_disconnect=on_disconnect
+                client.on_log=on_log
+                client.username_pw_set(username=MQTT_USERNAME, password=MQTT_PASSWORD)
+
+                print("connecting to broker ", broker, )
+
+                client.connect(broker, port=1883)
+                client.loop_start()
+                client.subscribe("carl.pettersson@abbindustrigymnasium.se/drive", 1)
+                client.publish("carl.pettersson@abbindustrigymnasium.se/drive", "S", 0)
 
                 client.loop_stop()
                 client.disconnect()
